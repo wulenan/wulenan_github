e954ba1e2fa3c6f6d669e9f82314aa4215b83fb0
cugb_wulenan <cugb_wulenan@sina.com>
2020-08-09 17:35:19 +0800

    delete!

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
         int work_feature3=3;
         int work_feature3_2=3;
         int develop=1;
-	string str("Hello World!!!!");
+	string str("Hello World!");
 	title out(str);
 	if(true)
 	{
