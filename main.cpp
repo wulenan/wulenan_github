7154e0f9390d13c9f0348f75e2a21cacac220efd
cugb_wulenan <cugb_wulenan@sina.com>
2020-08-09 09:28:48 +0800

    delete

--- main.cpp
+++ main.cpp
@@ -5,9 +5,6 @@ using std::cout;
 using std::endl;
 int main()
 {
-        int work_feature1=1;
-        int develop=1;
-        int develop=2;
 	string str("Hello World!!!!");
 	title out(str);
 	if(true)
@@ -15,7 +12,6 @@ int main()
 			title t;
 	}
 	cout<<"ifÖ´ĞĞÍê³É"<<endl;
-        int maj=100;
 	return 0;
 }
 
